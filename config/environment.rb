8c4f10a5ec3c1949a9f05ede38978160bfd04ce4
akianderson <s.aki.anderson@gmail.com>
2014-03-19 07:00:11 -0500

    heroku config

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 YAML.load_file("#{::Rails.root}/config/provider.yml")[::Rails.env].each {|k,v| ENV[k] = v } if Rails.env.development?
-YAML.load_file("app/#{::Rails.root}/config/provider.yml")[::Rails.env].each {|k,v| ENV[k] = v } if Rails.env.production?
+YAML.load_file("#{::Rails.root}/app/config/provider.yml")[::Rails.env].each {|k,v| ENV[k] = v } if Rails.env.production?
 
 
 # Initialize the Rails application.
