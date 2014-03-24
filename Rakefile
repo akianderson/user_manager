edc602d33a3819c28343fcb485793661acc313f3
akianderson <s.aki.anderson@gmail.com>
2014-03-23 20:32:49 -0500

    fix rake file

--- Rakefile
+++ Rakefile
@@ -3,4 +3,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-GrumpyNews::Application.load_tasks
+UserManager::Application.load_tasks
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-# 	provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-# 	provider :fitbit, ENV['FITBIT_KEY'], ENV['FITBIT_SECRET']
-# 	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}, :provider_ignores_state => true}
-# 	provider :developer
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+	provider :fitbit, ENV['FITBIT_KEY'], ENV['FITBIT_SECRET']
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}, :provider_ignores_state => true}
+	provider :developer
+end
